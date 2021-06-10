28e45efe61f5062205eaefed289fb71e878248ae
Michael <Mlcorrieri@gmail.com>
2021-06-10 17:27:32 -0400

    need to fix post controller create Thur 530pm

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,24 @@ class PostsController < ApplicationController
         render json: @post
     end
 
+    def create 
+        @post = Post.new(post_params)
+
+        if @post.valid?
+            @post.save 
+            render json: @post 
+        else 
+            render json: {error: "cannot create post"}
+        end 
+    end 
+
+    def destroy
+        @post.destroy
+    end
+
+    private
+    def post_params
+        params.require(:post).permit(:date, :description, :image, :water_type, :location)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   
-  resources :users, only: [:create]
+  resources :users, only: [:create, :index]
   post "/login", to: "users#login"
   
   
-  resources :posts, only: [:show, :index]
+  resources :posts, only: [:show, :index, :create, :update, :delete]
   resources :fish, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
