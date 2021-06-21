60be83357861f4118295f390d29ae91dc5684683
Michael <Mlcorrieri@gmail.com>
2021-06-21 11:26:27 -0400

    upost serializer sends fish needed for search

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,6 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :date, :description, :image, :location, :price, :comment, :user_id, :fish_id 
+
+
+  belongs_to :fish
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   post "/login", to: "users#login"
   get "/keep_logged_in", to: "users#keep_logged_in"
   get "/users/:id/fish", to: "fish#get_fish_by_owner"
-  
-  
+
+
   
   resources :users
   resources :posts
